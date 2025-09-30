5f43bb81ec5c772253c37df2444d19bcde3a6b4d
Martina <martina.dejesus@despegar.com>
2025-09-30 19:58:05 -0300

    first commit

---
+++ src/main/kotlin/com/ingsis_team/permission_service_2025/PermissionService2025Application.kt
@@ -0,0 +1,11 @@
+package com.ingsis_team.permission_service_2025
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PermissionService2025Application
+
+fun main(args: Array<String>) {
+	runApplication<PermissionService2025Application>(*args)
+}
---
+++ src/test/kotlin/com/ingsis_team/permission_service_2025/PermissionService2025ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ingsis_team.permission_service_2025
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PermissionService2025ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
